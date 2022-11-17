d4a471fb122cde4f9373aab43ca07e55e79686ed
Let's Do Tech <sumeet@letsdotech.dev>
2022-11-17 19:36:04 +0100

    init

--- provider.tf
+++ provider.tf
@@ -5,4 +5,11 @@ terraform {
       version = "~> 4.18.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "spaceliftenv-tfstate"
+    key            = "terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "spacelift_tf_lockid"
+  }
 }
